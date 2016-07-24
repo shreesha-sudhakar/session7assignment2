83957aef21ec03e013aea9d0d5dedd184f477c25
shreesha-sudhakar <shreeshasudhakar3@gmail.com>
2016-07-25 00:44:49 +0530

    Add files via upload

---
+++ hideoddrows.js
@@ -0,0 +1,28 @@
+function hideoddrows()
+{
+
+	if(document.getElementById("odd").style.display=='none')
+	{
+		document.getElementById("odd").style.display = '';
+	}
+	else
+	{
+		document.getElementById("odd").style.display='none';	
+	}
+	if(document.getElementById("odd1").style.display=='none')
+	{
+		document.getElementById("odd1").style.display = '';
+	}
+	else
+	{
+		document.getElementById("odd1").style.display='none';	
+	}
+	if(document.getElementById("odd2").style.display=='none')
+	{
+		document.getElementById("odd2").style.display = '';
+	}
+	else
+	{
+		document.getElementById("odd2").style.display='none';	
+	}
+}
